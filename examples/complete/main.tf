1be7ddb79d540b3b6615807c6df8220f0f3bfb3a
Jorge Gomez Velasquez <jorgegomez2307@hotmail.com>
2022-10-11 13:37:46 -0500

    Create route map for route control module (#1)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,8 +1,22 @@
-module "aci_scaffolding" {
-  source  = "netascode/scaffolding/aci"
+module "aci_route_control_route_map" {
+  source  = "netascode/route-control-route-map/aci"
   version = ">= 0.0.1"
 
   name        = "ABC"
-  alias       = "ABC-ALIAS"
   description = "My Description"
+  tenant      = "TEN1"
+  contexts = [
+    {
+      name        = "CTX1"
+      description = "My Context 1"
+      action      = "deny"
+      order       = 1
+      set_rule    = "SET1"
+      match_rules = ["MATCH1"]
+    },
+    {
+      name        = "CTX2"
+      match_rules = ["MATCH2", "MATCH3"]
+    }
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  description = "Tenant name."
+  description = "Route Control Route Map name."
   type        = string
 
   validation {
@@ -8,19 +8,18 @@ variable "name" {
   }
 }
 
-variable "alias" {
-  description = "Tenant alias."
+variable "tenant" {
+  description = "Route Control Route Map Tenant name."
   type        = string
-  default     = ""
 
   validation {
-    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.alias))
+    condition     = can(regex("^[a-zA-Z0-9_.-]{0,64}$", var.tenant))
     error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
   }
 }
 
 variable "description" {
-  description = "Tenant description."
+  description = "Route Control Route Map description."
   type        = string
   default     = ""
 
@@ -28,4 +27,53 @@ variable "description" {
     condition     = can(regex("^[a-zA-Z0-9\\!#$%()*,-./:;@ _{|}~?&+]{0,128}$", var.description))
     error_message = "Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `\\`, `!`, `#`, `$`, `%`, `(`, `)`, `*`, `,`, `-`, `.`, `/`, `:`, `;`, `@`, ` `, `_`, `{`, `|`, }`, `~`, `?`, `&`, `+`. Maximum characters: 128."
   }
+}
+
+variable "contexts" {
+  description = "Route Control Route Map contexts. Allowed values `action`:  `deny` or `permit`. Allowed values `order`: 0-9"
+  type = list(object({
+    name        = string
+    description = optional(string, "")
+    action      = optional(string, "permit")
+    order       = optional(number, 0)
+    set_rule    = optional(string, "")
+    match_rules = optional(list(string), [])
+  }))
+  default = []
+
+  validation {
+    condition = alltrue([
+      for ctx in var.contexts : ctx.name == null || can(regex("^[a-zA-Z0-9_.-]{0,64}$", ctx.name))
+    ])
+    error_message = "Context `name`: Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+  }
+
+  validation {
+    condition = alltrue([
+      for ctx in var.contexts : ctx.description == null || can(regex("^[a-zA-Z0-9\\!#$%()*,-./:;@ _{|}~?&+]{0,128}$", ctx.description))
+    ])
+    error_message = "Context `description`: Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `\\`, `!`, `#`, `$`, `%`, `(`, `)`, `*`, `,`, `-`, `.`, `/`, `:`, `;`, `@`, ` `, `_`, `{`, `|`, }`, `~`, `?`, `&`, `+`. Maximum characters: 128."
+  }
+
+  validation {
+    condition = alltrue([
+      for ctx in var.contexts : ctx.action == null || contains(["deny", "permit"], ctx.action)
+    ])
+    error_message = "Context `action`: Valid values are `deny` or `permit`."
+  }
+
+  validation {
+    condition = alltrue([
+      for ctx in var.contexts : ctx.order >= 0 && ctx.order <= 9
+    ])
+    error_message = "Allowed values Context `order`: 0-9."
+  }
+
+  validation {
+    condition = alltrue([
+      for ctx in var.contexts : ctx.set_rule == null || can(regex("^[a-zA-Z0-9_.-]{0,64}$", ctx.set_rule))
+    ])
+    error_message = "Context Set rule `name`: Allowed characters: `a`-`z`, `A`-`Z`, `0`-`9`, `_`, `.`, `-`. Maximum characters: 64."
+  }
+
 }

6bae289d956735f799f594ed10b5f3fa8ac71821
danischm <danischm@cisco.com>
2023-04-08 09:38:52 +0200

    Fix resource dependencies

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aci_rest_managed" "rtctrlProfile" {
 
 resource "aci_rest_managed" "rtctrlCtxP" {
   for_each   = { for ctx in var.contexts : ctx.name => ctx }
-  dn         = "${aci_rest_managed.rtctrlProfile.id}/ctx-${each.value.name}"
+  dn         = "${aci_rest_managed.rtctrlProfile.dn}/ctx-${each.value.name}"
   class_name = "rtctrlCtxP"
   content = {
     name   = each.value.name
@@ -34,7 +34,7 @@ resource "aci_rest_managed" "rtctrlCtxP" {
 
 resource "aci_rest_managed" "rtctrlScope" {
   for_each   = { for ctx in var.contexts : ctx.name => ctx if ctx.set_rule != "" }
-  dn         = "${aci_rest_managed.rtctrlCtxP[each.value.name].id}/scp"
+  dn         = "${aci_rest_managed.rtctrlCtxP[each.value.name].dn}/scp"
   class_name = "rtctrlScope"
   content = {
     descr = ""
@@ -43,7 +43,7 @@ resource "aci_rest_managed" "rtctrlScope" {
 
 resource "aci_rest_managed" "rtctrlRsScopeToAttrP" {
   for_each   = { for ctx in var.contexts : ctx.name => ctx if ctx.set_rule != "" }
-  dn         = "${aci_rest_managed.rtctrlScope[each.value.name].id}/rsScopeToAttrP"
+  dn         = "${aci_rest_managed.rtctrlScope[each.value.name].dn}/rsScopeToAttrP"
   class_name = "rtctrlRsScopeToAttrP"
   content = {
     tnRtctrlAttrPName = each.value.set_rule
@@ -52,7 +52,7 @@ resource "aci_rest_managed" "rtctrlRsScopeToAttrP" {
 
 resource "aci_rest_managed" "rtctrlRsCtxPToSubjP" {
   for_each   = { for match in local.context_match_rules : match.match_rule => match }
-  dn         = "${aci_rest_managed.rtctrlCtxP[each.value.ctx].id}/rsctxPToSubjP-${each.value.match_rule}"
+  dn         = "${aci_rest_managed.rtctrlCtxP[each.value.ctx].dn}/rsctxPToSubjP-${each.value.match_rule}"
   class_name = "rtctrlRsCtxPToSubjP"
   content = {
     tnRtctrlSubjPName = each.value.match_rule
